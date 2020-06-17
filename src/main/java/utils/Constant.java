0c673ba498abddb6ac34cb3131b016ee82d84a7e
mindtree348 <mindtree348@mindtree96.iiht.tech>
2020-06-17 22:33:57 +0530

    Update

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -8,7 +8,6 @@ public class Constant {
 	public final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\drivers\\geckodriver.exe";
 	
 	public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\drivers\\chromedriver.exe";
-//	public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "/src/test/java/resources/drivers/chromedriver.exe";
 	
 	public final static String IE_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\drivers\\IEDriverServer.exe";
 }
